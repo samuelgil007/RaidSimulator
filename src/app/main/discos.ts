8089fbeb8eb3b1466a4fdb31001a318befc839da
“samuelgil007” <sagilar15@gmail.com>
2020-12-23 16:57:44 -0500

    datos corregidos

--- src/app/main/discos.ts
+++ src/app/main/discos.ts
@@ -74,145 +74,145 @@ export let discos = {
     "tipo2": {
         "16": {
             "nombre": "Toshiba MG08ACA16TE",
-            "capacidad": "16",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "512"
+            "capacidad": 16,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 512
         },
         "14": {
             "nombre": "TOSHIBA MG07ACA14TE",
-            "capacidad": "14",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "256"
+            "capacidad": 14,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 256
         },
         "12": {
             "nombre": "TOSHIBA HDWG21CXZSTA",
-            "capacidad": "12",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "260"
+            "capacidad": 12,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 260
         },
         "10": {
             "nombre": "TOSHIBA MN06ACA10T",
-            "capacidad": "10",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 10,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "8": {
             "nombre": "TOSHIBA MN05ACA800",
-            "capacidad": "8",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 8,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "6": {
             "nombre": "TOSHIBA HDWE160",
-            "capacidad": "6",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 6,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "4": {
             "nombre": "TOSHIBA HDWE140",
-            "capacidad": "4",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 4,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "3": {
             "nombre": "TOSHIBA HDWD130",
-            "capacidad": "3",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 3,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "2": {
             "nombre": "TOSHIBA HDWD120",
-            "capacidad": "2",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 2,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "1": {
             "nombre": "Toshiba MQ04AB",
-            "capacidad": "1",
-            "Rotational delay": "5.56",
-            "Seek Time": "11.11",
-            "Disk transfer rate": "216.7"
+            "capacidad": 1,
+            "Rotational delay": 5.56,
+            "Seek Time": 11.11,
+            "Disk transfer rate": 216.7
         }
     },
     "tipo3": {
         "16": {
             "nombre": "WDC WUH721816ALE6L4",
-            "capacidad": "16",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 16,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "14": {
             "nombre": "WDC WUH721414ALE6L4",
-            "capacidad": "14",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "512"
+            "capacidad": 14,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 512
         },
         "12": {
             "nombre": "WDC WD121KRYZ",
-            "capacidad": "12",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 12,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "10": {
             "nombre": "WDC WD101KRYZ",
-            "capacidad": "10",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 10,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "8": {
             "nombre": "WDC WD8002FRYZ",
-            "capacidad": "8",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 8,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "6": {
-            "nombre": " WDC WD60EZRZ",
-            "capacidad": "6",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "nombre": "WDC WD60EZRZ",
+            "capacidad": 6,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "4": {
             "nombre": "WDC WD4004FZWX",
-            "capacidad": "4",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 4,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "3": {
             "nombre": "WDC WD30EURX",
-            "capacidad": "3",
-            "Rotational delay": "4.17",
-            "Seek Time": "8.33",
-            "Disk transfer rate": "600"
+            "capacidad": 3,
+            "Rotational delay": 4.17,
+            "Seek Time": 8.33,
+            "Disk transfer rate": 600
         },
         "2": {
             "nombre": "WDC WD20NMVW",
-            "capacidad": "2",
-            "Rotational delay": "5.77",
-            "Seek Time": "11.54",
-            "Disk transfer rate": "400"
+            "capacidad": 2,
+            "Rotational delay": 5.77,
+            "Seek Time": 11.54,
+            "Disk transfer rate": 400
         },
         "1": {
             "nombre": "WDC WD10TMVW",
-            "capacidad": "1",
-            "Rotational delay": "5.77",
-            "Seek Time": "11.54",
-            "Disk transfer rate": "400"
+            "capacidad": 1,
+            "Rotational delay": 5.77,
+            "Seek Time": 11.54,
+            "Disk transfer rate": 400
         }
     }
 }
