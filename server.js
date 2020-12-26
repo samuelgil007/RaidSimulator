9bd19abf5c47da8c357a290e5c448628ea45369f
“samuelgil007” <sagilar15@gmail.com>
2020-12-26 12:21:24 -0500

    Deploy v0.3

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/raidsimulator'));
+app.use(express.static(__dirname + '/dist/RaidSimulator'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/raidsimulator/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/RaidSimulator/index.html'));
 });
 
 // default Heroku port
